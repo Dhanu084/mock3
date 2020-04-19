5163f36a56e94548a65254d16c68582f54b42714
Dhanu084 <54932658+Dhanu084@users.noreply.github.com>
2020-04-19 19:01:52 +0530

    Update nodemailer.js

--- Mock3/config/nodemailer.js
+++ Mock3/config/nodemailer.js
@@ -8,8 +8,8 @@ let transporter = nodemailer.createTransport({
     port:587,
     secure:false,
     auth:{
-        user:'dhanushkiran902@gmail.com',
-        pass:'Beastslayer2$'
+        user:'',
+        pass:''
     }
 });
 
